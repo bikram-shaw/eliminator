f34c97cb9f783df51e9d1e53bcfaf8e275dc969e
bikramshawbnk@gmail.com <bikramshawbnk@gmail.com>
2021-04-02 01:48:12 +0530

    from mess

--- app/src/main/java/com/example/eliminator/activities/Home.java
+++ app/src/main/java/com/example/eliminator/activities/Home.java
@@ -6,16 +6,30 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.eliminator.R;
+import com.squareup.picasso.Picasso;
 
 public class Home extends AppCompatActivity {
     private CardView ffsds,ffcs,fft;
+    ImageView ffsds_pic,ffcs_pic,fft_pic;
     Intent intent;
-
+public void loadHomeImages(){
+    ffsds_pic=findViewById(R.id.home_ffsds);
+    ffcs_pic=findViewById(R.id.home_ffcs);
+    fft_pic=findViewById(R.id.home_fft);
+    Picasso.get().load("https://eliminator-images.s3.ap-south-1.amazonaws.com/home_images/ffcs.jpg")
+            .into(ffcs_pic);
+    Picasso.get().load("https://eliminator-images.s3.ap-south-1.amazonaws.com/home_images/ffsds.jpg")
+            .into(ffsds_pic);
+    Picasso.get().load("https://eliminator-images.s3.ap-south-1.amazonaws.com/home_images/fft.jpg")
+            .into(fft_pic);
+}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
         ffsds=findViewById(R.id.home_ffsds_card);
         ffcs=findViewById(R.id.home_ffcs_card);
@@ -47,5 +61,6 @@ public class Home extends AppCompatActivity {
 
             }
         });
+        loadHomeImages();
     }
 }
